c8fb67a7f5a42ea700a4800f18454adf6e1145e1
sirokuro171 <sirokuro171@example.com>
2025-12-20 06:06:13 +0900

    flake nixpkgsを更新

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "mod-audio for nix system";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
   };
 
   outputs =
