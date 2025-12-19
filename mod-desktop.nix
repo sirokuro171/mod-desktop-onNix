135117557794cdc9f0cd52429d880720bd9d37d4
sirokuro171 <sirokuro171@example.com>
2025-12-20 06:02:57 +0900

    makeWrapperを使用

--- mod-desktop.nix
+++ mod-desktop.nix
@@ -5,76 +5,90 @@
   fetchzip,
   qt5,
   autoPatchelfHook,
+  makeWrapper,
 }:
 let
-  bash = pkgs.bashNonInteractive;
-in
-qt5.mkDerivation rec {
   pname = "mod-desktop";
   version = "0.0.12";
 
-  src = fetchzip {
-    url = "https://github.com/mod-audio/${pname}/releases/download/${version}/${pname}-${version}-linux-x86_64.tar.xz";
-    hash = "sha256-MXGVgjjWuBy0bX528asX6pbR7ptQTHQy4Zd/GVgtQyo=";
+  # Build unwrapped
+  unwrapped = qt5.mkDerivation {
+    pname = "${pname}-unwrapped";
+    version = "${version}";
+
+    src = fetchzip {
+      url = "https://github.com/mod-audio/${pname}/releases/download/${version}/${pname}-${version}-linux-x86_64.tar.xz";
+      hash = "sha256-MXGVgjjWuBy0bX528asX6pbR7ptQTHQy4Zd/GVgtQyo=";
+    };
+
+    buildInputs =
+      let
+        deps = with pkgs; [
+          alsa-lib
+        ];
+      in
+      [
+        stdenv.cc.libc
+        qt5.qtbase
+        qt5.qtwayland
+      ]
+      ++ deps;
+
+    nativeBuildInputs = [ autoPatchelfHook ];
+
+    dontWrapGApps = true;
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir $out
+      cp -r ./mod-desktop $out/mod-desktop
+
+      runHook postInstall
+    '';
   };
+in
+# Build wrapped
+stdenv.mkDerivation {
+  pname = "${pname}";
+  version = "${version}";
 
-  buildInputs =
-    let
-      deps = with pkgs; [
-        alsa-lib
-      ];
-    in
-    [
-      stdenv.cc.libc
-      qt5.qtbase
-      bash
-    ]
-    ++ deps;
-
+  buildInputs = [ unwrapped ];
   nativeBuildInputs = [
+    makeWrapper
     qt5.wrapQtAppsHook
-    autoPatchelfHook
   ];
 
-  buildPhase =
-    let
-      qt = qt5.qtbase.bin;
-    in
-    ''
-      runHook preBuild
+  dontWrapQtApps = true;
 
-      cat << EOF1 > mod-desktop.desktop
-      [Desktop Entry]
-      Categories=AudioVideo;X-AudioEditing;Qt;
-      Exec=$out/bin/mod-desktop
-      GenericName=MOD Desktop
-      Icon=audio
-      Name=MOD Desktop
-      Terminal=false
-      Type=Application
-      Version=1.0
-      EOF1
+  dontUnpack = true;
+  dontConfigure = true;
+  dontFixup = true;
 
-      cat << EOF2 > mod-desktop.run
-      #!${bash}/bin/bash
-
-      export QT_PLUGIN_PATH=${qt}/lib/qt-${qt.version}/plugins
-
-      cd "$out/mod-desktop"
-      exec ./mod-desktop
-      EOF2
-
-      runHook postBuild
-    '';
+  buildPhase = ''
+    cat << EOF > mod-desktop.desktop
+    [Desktop Entry]
+    Categories=AudioVideo;X-AudioEditing;Qt;
+    Exec=$out/bin/mod-desktop
+    GenericName=MOD Desktop
+    Icon=audio
+    Name=MOD Desktop
+    Terminal=false
+    Type=Application
+    Version=1.0
+    EOF
+  '';
 
   installPhase = ''
-    runHook preInstall
+    local makeWrapperArgs=(
+      "''${qtWrapperArgs[@]}"
+      --chdir ${unwrapped}/mod-desktop
+    )
+    echo "''${makeWrapperArgs[@]}"
 
     mkdir $out
-    cp -r ./mod-desktop $out/mod-desktop
-    install -Dm555 mod-desktop.run $out/bin/mod-desktop
     install -Dm444 mod-desktop.desktop $out/share/applications/mod-desktop.desktop
-
-    runHook postInstall
+    makeWrapper ${unwrapped}/mod-desktop/mod-desktop $out/bin/mod-desktop "''${makeWrapperArgs[@]}"
   '';
+
 }
