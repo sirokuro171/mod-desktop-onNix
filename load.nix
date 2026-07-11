eacd2a8f0abcbc9d5b47ea1adc9e001fea870b59
sirokuro171 <sirokuro171@example.com>
2026-07-12 02:42:16 +0900

    attr読み込み実装

--- load.nix
+++ load.nix
@@ -1,11 +1,50 @@
 pkgs:
 let
-  packages =
+  # default.nixを探索
+  defaults =
     let
       dirs = pkgs.lib.filterAttrs (
         n: v: v == "directory" && builtins.pathExists (./pkgs + "/${n}/default.nix")
       ) (builtins.readDir ./pkgs);
     in
-    pkgs.lib.mapAttrs (n: v: ./pkgs + "/${n}/default.nix") dirs;
+    builtins.mapAttrs (n: v: ./pkgs + "/${n}/default.nix") dirs;
+
+  loader =
+    key: value:
+    let
+      loaded = import value;
+
+      ## attrを読み込む
+      attr_loader =
+        n: v:
+        if builtins.isFunction v then
+          pkgs.callPackage v {}
+        else if builtins.isAttrs v then
+          ## 再帰的に読み込み、nullを除外
+          pkgs.lib.filterAttrs (n: v: v != null) (builtins.mapAttrs attr_loader v)
+        else
+          null;
+    in
+    if builtins.isFunction loaded then
+      { "${key}" = pkgs.callPackage loaded {}; }
+    else if builtins.isAttrs loaded then
+      builtins.mapAttrs attr_loader loaded
+    else
+      null;
+
+  # default.nixをimport
+  result = pkgs.lib.attrsets.mapAttrsToList loader defaults;
+
+  # attrのみを抽出
+  filtered = builtins.filter (e: builtins.isAttrs e) result;
+
+  #   packages =
+  #     let
+  #       dirs = pkgs.lib.filterAttrs (
+  #         n: v: v == "directory" && builtins.pathExists (./pkgs + "/${n}/default.nix")
+  #       ) (builtins.readDir ./pkgs);
+  #       default_list = pkgs.lib.mapAttrs (n: v: ./pkgs + "/${n}/default.nix") dirs;
+  #     in
+  #     pkgs.lib.mapAttrs (n: v: pkgs.callPackage v { });
 in
-pkgs.lib.mapAttrs (n: v: pkgs.callPackage v { }) packages
+pkgs.lib.attrsets.mergeAttrsList filtered
